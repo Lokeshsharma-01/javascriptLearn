5310afe02cafb3a96d5755e361ef3b93ff20ca0c
Lokeshsharma-01 <iamlokeshsharmaa@gmail.com>
2023-10-15 12:46:21 +0530

    string and string methods

---
+++ StringsMethods-Day3.js
@@ -0,0 +1,25 @@
+// String length
+// String slice()
+// String substring()
+// String substr()
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String charAt()
+// String charCodeAt()
+// String split()
+
+
+let string = "Lokesh-Sharma-Is-a-coder    "
+console.log(string.length)
+//                start--| |--end
+console.log(string.slice(2,9))
+
+console.log(string.substring(5));
